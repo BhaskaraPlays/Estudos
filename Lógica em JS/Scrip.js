c637c816356680aa6c8e400dffda00e51b04f31f
BhaskaraPlays <128322538+BhaskaraPlays@users.noreply.github.com>
2026-05-14 10:10:21 -0300

    Add files via upload

---
+++ Lógica em JS/Scrip.js
@@ -0,0 +1,9 @@
+// Example JavaScript code
+import "./styles.css";
+
+function sayHello(name) {
+    return `Hello, ${name}!`;
+}
+console.clear
+const message = sayHello('World');
+console.log(message)
